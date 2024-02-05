9427f31d4e75a465d5b4c4bdec0bc1edfab2cf27
Muthmainah1 <128261655+Muthmainah1@users.noreply.github.com>
2024-02-05 21:54:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,12 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
         { name: 'card4', img: 'images/rollsafe.png' },
         { name: 'card5', img: 'images/success.png' },
         { name: 'card5', img: 'images/success.png' },
-        { name: 'card6', img: 'images/baby.jpg' }, 
-        { name: 'card6', img: 'images/baby.jpg' },
-        { name: 'card7', img: 'images/cat.jpg' },  
-        { name: 'card7', img: 'images/cat.jpg' },
-        { name: 'card8', img: 'images/dog.jpg' }, 
-        { name: 'card8', img: 'images/dog.jpg' },
         // ...add more pairs as needed
     ];
 
